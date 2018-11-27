c77dd99296f5ee054faa405b283b6bc48e274f5f
weston <boldt.weston@gmail.com>
2018-11-26 23:30:46 -0600

    chmod +x & search_stackoverflow
    just a script i've had in my bin for awhile that i used one time 5 years
    ago that i thought i'd include somewhere for those l33t hackers using
    lynx

---
+++ search_stackoverflow.sh
@@ -0,0 +1,8 @@
+# for when you are very stuck
+search_stackoverflow() {
+    search=""
+    for term in $@; do
+        search="$search%20$term"
+    done
+    lynx "https://stackoverflow.com/search?q=$search"
+}
